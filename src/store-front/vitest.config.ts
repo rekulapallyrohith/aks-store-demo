51de0aa4bfff40f03ce5172704daa9b41042b0c4
rekulapallyrohith <rekulapallyrohith17@gmail.com>
2025-09-17 15:20:06 +0530

    fixed3

--- src/store-front/vitest.config.ts
+++ src/store-front/vitest.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vitest/config'
 import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
   plugins: [vue()],
   test: {
     environment: 'jsdom',
     exclude: ['e2e/**', 'node_modules/**'],
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
   }
 })
+
