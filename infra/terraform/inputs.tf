7c385141b238b2a2e8f75c9b81bb1f97829cea15
rekulapallyrohith <rekulapallyrohith17@gmail.com>
2025-12-06 09:57:39 +0530

    added pr.yaml (#2)

---
+++ infra/terraform/inputs.tf
@@ -0,0 +1,33 @@
+variable "resource_group_info" {
+    type = object({
+      name = string
+      location = string
+    })
+    default = {
+      name = "myresourcegroup"
+      location = "eastus"
+    }
+  
+}
+
+variable "aks_cluster_info" {
+    type = object({
+      name = string
+      count = number
+      size = string
+      version = string
+    })
+    default = {
+      name = "aksshopcluster-qa"
+      count = 3
+      size = "Standard_B2ms"
+      version = "1.31"
+    }
+  
+}
+
+variable "set_kubeconfig" {
+    type = string
+    default = "1.0"
+  
+}
